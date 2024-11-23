a3ec21cd94e866000f33c462cb73d72a2576348d
Fosberg Addai <fosberg1addai@gmail.com>
2024-11-23 04:09:12 -1100

    just shipped this

--- guessgame/src/main.rs
+++ guessgame/src/main.rs
@@ -14,11 +14,13 @@ let mut guessnum = String::new();
 io::stdin().read_line(&mut guessnum).expect("Error in reading your guess number");
 // println!("your guessed number is {}",guessnum);
 
-let guessnumb: i32 = match guessnum.trim().parse(){
-Ok(num) => num
-Err(_) => { println!("Please enter a valid number")
-return }
-}
+let guessnumb:u32 = guessnum.trim().parse().expect("Please type a valid number");
+
+// let guessnumb: i32 = match guessnum.trim().parse(){
+// Ok(num) => num
+// Err(_) => { println!("Please enter a valid number")
+// return }
+// }
 
 println!("Yoour guessed numnber is {randomnum}");
 
@@ -26,7 +28,9 @@ if randomnum == guessnumb {
     println!("you won!! with machine guess {guessnum}");
 } else {
     println!("You Lost!! Machine says guess is  {guessnum}, try again");
+
 }
+
 
 
 
