f558eb7fd0db1af4abb2a1a9baeb0e22c596f079
Fosberg Addai <fosberg1addai@gmail.com>
2024-11-23 03:44:03 -1100

    Pushing hard

--- guessgame/src/main.rs
+++ guessgame/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 
 fn main(){
@@ -6,9 +7,29 @@ fn main(){
 println!("Welcome to my number guess game");
 println!("Enter any number between 1 - 100");
 
+let randomnum = rand::thread_rng().gen_range(1..=100);
+
 let mut guessnum = String::new();
 
 io::stdin().read_line(&mut guessnum).expect("Error in reading your guess number");
-println!("your guessed number is {}",guessnum);
+// println!("your guessed number is {}",guessnum);
+
+let guessnumb: i32 = match guessnum.trim().parse(){
+    Ok(num) => num,
+    Err(_) =>{
+        println!("Please enter a valid number!");
+        return;
+    }
+};
+
+println!("Yoour guessed numnber is {randomnum}");
+
+if randomnum == guessnumb {
+    println!("you won!! with machine guess {guessnum}");
+} else {
+    println!("You Lost!! Machine says guess is  {guessnum}, try again");
+}
+
+
 
 }
