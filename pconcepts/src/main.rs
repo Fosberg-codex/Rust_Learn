ae5379083d5b7f8856a82fcb1ce564bfac885f86
Fosberg Addai <fosberg1addai@gmail.com>
2024-11-25 08:36:44 -1100

    done something small today

---
+++ pconcepts/src/main.rs
@@ -0,0 +1,39 @@
+// Programming concepts in rust
+//functions and their return types
+
+fn win() ->i32{
+    778
+}
+
+
+fn main() {
+let  mut x = 6;
+println!("printing {x}");
+
+// we call this the inner scope
+{
+    x = x + 67;
+println!("printing new {x}");
+}
+
+
+foo(89);
+
+let jake = win();
+
+
+//Data types
+//we have two types of compilers that is the scaler type and the compound type
+
+//Scaler type
+//Scaler type represnets a single type. Rust scaler type values are integers, floating point values, boolena and characters and 
+println!("The win number is {jake}");
+loop{
+    println!("Great")
+}
+}
+
+fn foo(y:i32){
+ println!("I am the new {y}");
+}
+
